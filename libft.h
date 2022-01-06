471915451d4c08fdc8c03582b0ca91ead5d5025c
marrodri <laza.mar64@gmail.com>
2022-01-05 23:52:40 -0800

    chore: updated libft.h

--- libft.h
+++ libft.h
@@ -11,10 +11,33 @@ typedef struct	s_list{
 	struct s_list	*next;
 }				t_list;
 
+/*
+** Boolean Utils
+*/
 int ft_isalpha(int c);
 int ft_isdigit(int d);
 int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
+
+/*
+** String Utils
+*/
 int ft_strlen(char *str);
+char *ft_strlcpy();
+char *strlcat();
+char *ft_toupper();
+char *ft_strdup(char *str); 
+/*
+** Memory Utils
+*/
+
+void *ft_memset();
+void *ft_memcpy();
+void ft_bzero();
+void *ft_calloc(size_t num, size_t size);
+/*
+** Linked List Utils
+*/
+
 #endif
