285c1284f62b03bc9b83bd06471b2a8ad27408b5
marrodri <laza.mar64@gmail.com>
2022-01-24 18:23:05 -0800

    chore: lstclear added and finish, and lstdelone updated :)

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -3,12 +3,8 @@
 
 void ft_lstdelone(t_list *lst, void (*del)(void *)){
 	t_list *top_head = lst;
-	// if(lst->next){
-	// 	r
-	// }
 	lst = lst->next;
-	//del clear the content of the node;
-	del(top_head);	
-	//free the memory of head.
+	del(top_head->content);	
 	free(top_head);
+	top_head = NULL;
 }
